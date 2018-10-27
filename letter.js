eb7676da7879cc462bf5dd137b9fc76a5f25b7e0
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-10-27 00:23:17 -0400

    attempt at start

---
+++ letter.js
@@ -0,0 +1,17 @@
+var Letter = function() {
+    this.underlyer = "_";
+    this.guess = false;
+
+    this.character = function() {
+        if (this.guess = true) {
+            console.log("correct letter")
+        }       
+        else {
+            console.log("_")
+        }
+    };
+
+    this.check = function() {
+        if ()
+    };
+};
