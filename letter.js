041a5d53df8b43eb0b0e080edceed78b0fdc55b0
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-11-03 14:15:51 -0400

    stuff

--- letter.js
+++ letter.js
@@ -1,17 +1,30 @@
+var word = require("./word.js");
+
 var Letter = function() {
     this.underlyer = "_";
     this.guess = false;
 
     this.character = function() {
         if (this.guess = true) {
-            console.log("correct letter")
+            console.log("you guessed")
+            // for (var i = 0; i < numBlanks; i++) {
+            //     if (chosenWord[i] === letter) {
+            //         letterInWord = true;
+            //     }
+            // }
         }       
         else {
-            console.log("_")
+            console.log("TYPE SOMETHING PLEASE!")
         }
     };
 
     this.check = function() {
-        if ()
+        if (this.underlyer = true) {
+            console.log("you guessed correct")
+            
+        }
+        else {
+
+        }
     };
 };
---
+++ word.js
@@ -0,0 +1,13 @@
+var letter = require("./letter.js");
+
+//array to hold letters in the underlying word
+var letters = new Letter (),
+
+//function that returns a string representing the word
+var shit = function() {
+    return(" ")
+    Letter.character();
+};
+
+//takes character as an argument, calls this.check function from letter.js
+var crap = function ()
